5f00ba1f9a0dccf8038a0968126e1fd32a55e9c6
CHARAN PRAJWAL <110123865+CHARANPRAJWAL@users.noreply.github.com>
2023-12-08 13:55:47 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 # config-repo/env.sh
 
-echo API_KEY=$API_KEY >> .env
-echo DATABASE_URL=$DATABASE_URL >> .env
+echo "API Key: $API_KEY" >> .env
+echo "Database URL: $DATABASE_URL" >> .env
 ls -la
 cat .env
